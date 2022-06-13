285217f8144d6e873c2182fd09937090f60ef184
Nick Molina <Nicholasmolina15@gmail.com>
2022-06-12 23:08:32 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import {Router} from "react-router-dom";
-import Data from './data/Data';
+import {Route} from "react-router-dom";
+import React from "react"
+// import Data from './data/Data';
+import Home from './pages/Home';
+import About from './pages/About';
+import Nav from "./components/Nav";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Route exact path="/">
         <Home/>
       </Route>
-      <Route exact path="/about">
+     <Route exact path="/about">
         <About/>
       </Route>
-      <Route exact path="stocks">
+      {/* <Route exact path="/stocks">
         <Stocks/>
-      </Route>
+      </Route>  */}
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+       <div className= "nav">
+    <Link to ='/'>
+        <div>Home</div>
+    </Link>
+    <Link to ='/stocks'>
+        <div>Stocks</div>
+    </Link>
+    <Link to ='/about'>
+        <div>About</div>
+    </Link>
+        </div>
+
+    )
+
+}
+
+export default Nav;
