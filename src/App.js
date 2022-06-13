db12a49a0b22b4caf7ebd08668b8638ba665ebd3
Nick Molina <Nicholasmolina15@gmail.com>
2022-06-12 22:55:05 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {Router} from "react-router-dom";
+import Data from './data/Data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route exact path="/about">
+        <About/>
+      </Route>
+      <Route exact path="stocks">
+        <Stocks/>
+      </Route>
     </div>
   );
 }
--- src/data/Data.js
+++ src/data/Data.js
@@ -1,5 +1,5 @@
 
-const data = [
+const Data = [
     {name: "Apple Inc.", symbol: "AAPL", lastPrice: 140.64, change: -0.280000000000001, high: 141.74, low: 140.35, open: 141.5},
     {name: "Microsoft Corporation", symbol: "MSFT", lastPrice: 64.98, change: 0.109999999999999, high: 65.45, low: 64.76, open: 65.12},
     {name: "Alphabet Inc.", symbol: "GOOGL", lastPrice: 835.14, change: -4.50999999999999, high: 844, low: 829.1, open: 842},
@@ -8,4 +8,4 @@ const data = [
     {name: "Intel Corporation", symbol: "INTL", lastPrice: 36.16, change: -0.370000000000005, high: 36.78, low: 36.125, open: 36.58}
   ]
 
-  export default data;
+  export default Data;
